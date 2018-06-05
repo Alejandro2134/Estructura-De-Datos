c30c1af0be1efcbbf1c4f260a7bd66ac60dbd1a6
Alejandro Zapata Molina <32116372+Alejandro2134@users.noreply.github.com>
2018-06-05 18:11:46 -0500

    Add files via upload

---
+++ Hackerearth/Little Monk and Goblet of Fire/src/Node.java
@@ -0,0 +1,17 @@
+
+public class Node {
+	
+	public int number;
+	public int rollNumber;
+	
+	public Node next;
+	
+	public Node () {}
+	
+	public Node (int number, int rollNumber)
+	{
+		this.number = number;
+		this.rollNumber = rollNumber;
+	}
+		
+}
