194508357fcfe0fd7df8a81484d87b3f57c67875
Alejandro Zapata Molina <32116372+Alejandro2134@users.noreply.github.com>
2018-06-05 18:26:20 -0500

    Add files via upload

---
+++ Hackerearth/Monk and Chamber of Secrets/src/Node.java
@@ -0,0 +1,17 @@
+
+public class Node {
+	
+	public int number;
+	public int position;
+	
+	public Node next;
+	
+	public Node () {}
+	
+	public Node (int number, int position)
+	{
+		this.number = number;
+		this.position = position;
+	}
+		
+}
