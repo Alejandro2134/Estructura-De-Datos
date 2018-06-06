e95e57743b07402c1ca213fb6660245a199e3d97
Victor <Victor@Victor->
2018-06-06 01:39:17 -0500

    Rush

--- HackerEarth/MonkandOrderofPhoenix/Logic.java
+++ HackerEarth/MonkandOrderofPhoenix/Logic.java
@@ -28,20 +28,6 @@ public class Logic {
 		return counter;
 	}
 			
-	public void insertAtIndex (Node newNode, int index)
-	{
-		Node temp;
-		Node pre = head;
-		
-		for(int i = 0; i < index-1; i++)
-			pre = pre.next;
-		
-		temp = pre.next;
-		pre.next = newNode;
-		newNode.next = temp;
-		
-	}
-	
 	public Node getNode (int index) throws IOException
 	{
 
@@ -55,30 +41,6 @@ public class Logic {
 			
 			return temp;
 		}
-		
-	}
-	
-	public void deleteAtIndex (int index)
-	{
-		Node temp;
-		Node pre = head;
-		
-		if(index != 0)
-		{
-			for(int i = 0; i < index-1; i++)
-				pre = pre.next;
-			
-			temp = pre.next;
-			pre.next = temp.next;
-			temp = null;
-			System.gc();
-		}
-		else
-		{
-			head = pre.next;
-			pre = null;
-			System.gc();
-		}		
 		
 	}
 	
@@ -98,28 +60,39 @@ public class Logic {
 		}
 	}
 	
+	public void deleteAtEnd ()
+	{
+		Node temp;
+		Node pre = head;
+		
+		while(pre.next.next != null)
+			pre = pre.next;
+		
+		temp = pre.next;
+		pre.next = null;
+		temp = null;
+		System.gc();
+	}
+	
 	public static void main(String[] args) {
-		
-		
-		Logic rows = new Logic ();
-		
+			
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		try {
 			
 			int n = Integer.parseInt(br.readLine());
-			int tam [] = new int[n];
+			Logic arrayList [] = new Logic [n];			//Arreglo de listas de tamaño n
 			
 			for(int i = 0; i < n; i++)
 			{
-				String input = br.readLine();
-				String input2 [] = input.split(" ");
+				String input [] = br.readLine().split(" ");
 	
-				tam[i] = Integer.parseInt(input2[0]);
+				arrayList[i] = new Logic();				//Se inicializa la lista en cada posición del arreglo
 				
-				for(int j = 1; j <= Integer.parseInt(input2[0]); j++)
-					rows.insertAtEnd(new Node(Integer.parseInt(input2[j])));			
+				for(int j = 0; j < Integer.parseInt(input[0]); j++)
+					arrayList[i].insertAtEnd(new Node(Integer.parseInt(input[j+1]))); //Se ingresan los datos de ingresados por
+																					  //consola en cada lista del arreglo
 			}
 			
 			int q = Integer.parseInt(br.readLine());
@@ -129,45 +102,45 @@ public class Logic {
 				String v = br.readLine();
 				String input [] = v.split(" ");
 				
-				if(input[0].equals("1"))
-				{
-					 if(Integer.parseInt(input[1]) == 1)
-					 {
-						 for(int j = 0; j < tam[0]; j++)
-						 {
-							 if(j == tam[0]-1)
-							 {
-								 rows.insertAtIndex(new Node(Integer.parseInt(input[2])), j+1);
-								 tam[0] += 1;
-								 break;
-							 }
-								
-						 }
-					 }
-					 else
-					 {
-						for(int j = tam[(Integer.parseInt(input[1]))-2]; j < tam[Integer.parseInt(input[1])-2] + tam[Integer.parseInt(input[1])-1]; j++ ) 
-						{
-							if(j == tam[Integer.parseInt(input[1])-2] + tam[Integer.parseInt(input[1])-1]-1)
-							{
-								rows.insertAtIndex(new Node(Integer.parseInt(input[2])), j+1);
-								tam[Integer.parseInt(input[1])-1] += 1;
-								break;
-							}
-						}
-					 }
-				}
-				else
+				if(input[0].equals("1"))		
+					arrayList[Integer.parseInt(input[1])-1].insertAtEnd(new Node(Integer.parseInt(input[2]))); //Se inserta un nuevo nodo	
+				else																						 //dependiendo de la lista en a que se va a ingresar 		
 					if(input[0].equals("0"))
-					{
-						
-					}
+						arrayList[Integer.parseInt(input[1])-1].deleteAtEnd();  //Se borra el ultimo nodo dependiendo de la lista en la que se va a quitar
 					else
 						if(v.equals("2"))
 						{
-							bw.write("YES");
-							bw.write("\n");
-						}
+							int min = Integer.MAX_VALUE;
+							int aux = 0;
+							
+							for(int j = 0; j < arrayList[0].sizeList(); j++) 		//Se recorre la primera lista buscando el minimo valor 				
+								if(arrayList[0].getNode(j).height < min)
+								{
+									min = arrayList[0].getNode(j).height;
+									aux = arrayList[0].getNode(j).height;
+								}
+																		  																										
+							for(int j = 1; j < arrayList.length; j++)				//Se compara el minimo valor con todas las demas listas
+							{
+								for(int k = 0; k < arrayList[j].sizeList(); k++)
+								{
+									if(arrayList[j].getNode(k).height == min+1)		//Si se encuentra un valor que sea una unidad mas grande que 
+										min = arrayList[j].getNode(k).height;		//el valor con el que se esta comparando se toma ese como nuevo valor
+								}
+								
+								if(aux == min)										//Si el valor "aux" es igual a el de la variable "min" es por que
+								{													//no se encontro número mayor
+									bw.write("NO \n");									
+									break;
+								}
+								else
+									aux = min;										//Si no el "aux" toma el valor del "min"
+								
+								if(j == arrayList.length-1)
+									bw.write("YES \n");
+							}
+							
+						}											  	
 				
 				bw.flush();
 			}
