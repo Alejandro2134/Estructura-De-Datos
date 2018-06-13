06ac43ab4fea7866149f888c623ecd58c93b169f
Familia <Familia@DESKTOP-GHECLVB>
2018-06-12 19:54:10 -0500

    Actualización

---
+++ HackerEarth/A_Tryst_With_Chess/Logic.java
@@ -0,0 +1,27 @@
+package A_Tryst_With_Chess;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Logic {
+
+	public static void main(String[] args) {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		try {
+			
+			int board [][] = new int [10][10];
+			
+			String [] input = br.readLine().split(" ");
+			
+			
+			
+		}catch (Exception ex) {}
+
+	}
+
+}
