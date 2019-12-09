e878b472820af419a9cbf14ce049f2f9bb85afc7
Alejandro Zapata Molina <zapatamolinaalejandro@gmail.com>
2019-12-09 17:48:03 -0500

    Ready

--- ProgrammingContestUNIMINUTO/ProblemWithLongName/Logic.java
+++ ProgrammingContestUNIMINUTO/ProblemWithLongName/Logic.java
@@ -1,10 +1,27 @@
 package ProblemWithLongName;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class Logic {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			
+			int t = sc.nextInt();
+			BigInteger base = BigInteger.valueOf(66);
+			BigInteger mod = BigInteger.valueOf(100);
+			
+			for(int i = 0; i < t; i++) {
+				BigInteger n = sc.nextBigInteger();
+				BigInteger result = base.modPow(n, mod);
+				System.out.println(result);
+			}
+			
+		} catch (Exception ex) {}
 	}
 
 }
