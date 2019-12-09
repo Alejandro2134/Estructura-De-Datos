154001b486e5564dd544c4793dd696bf3637e391
Alejandro Zapata Molina <zapatamolinaalejandro@gmail.com>
2019-12-09 16:45:36 -0500

    Programming Contest

---
+++ Estructura/ProgrammingContestUniminuto/CommonPermutation.java
@@ -0,0 +1,10 @@
+package ProgrammingContestUniminuto;
+
+public class CommonPermutation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Estructura/ProgrammingContestUniminuto/ReverseAndAdd.java
@@ -0,0 +1,10 @@
+package ProgrammingContestUniminuto;
+
+public class ReverseAndAdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
